842923db835bec880a1ec5688097d09e4b56b59d
MD Zillur Rahman <zillurdeu@gmail.com>
2023-06-23 09:40:08 +0200

    feat: basic server setup

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+// imports
+import express from "express";
+
+// rest object
+const app = express();
+const PORT = 8080;
+
+// routes
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1></h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`Application is running in port number ${PORT}`);
+});
