4717500b5adf943937dbf2f0f5d0a5c56d6defe8
MD Zillur Rahman <zillurdeu@gmail.com>
2023-06-24 14:12:01 +0200

    feat: hash password in the model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,6 +1,7 @@
 import mongoose, { Schema } from 'mongoose';
 import { IUser, IUserModel } from '../types/user.type';
 import validator from 'validator';
+import bcrypt from 'bcryptjs';
 const userSchema = new Schema<IUser>(
   {
     name: {
@@ -31,5 +32,11 @@ const userSchema = new Schema<IUser>(
     },
   },
 );
+
+// Hashing the password
+userSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 export const User = mongoose.model<IUser, IUserModel>('User', userSchema);
