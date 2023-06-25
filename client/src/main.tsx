2bf2a08914db7958a4a19f44f66f1b863f5a3dcd
MD Zillur Rahman <zillurdeu@gmail.com>
2023-06-25 21:26:55 +0200

    feat: setup redux toolkit

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,13 +5,17 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import { chakraTheme } from './styles/theme.ts';
+import { Provider } from 'react-redux';
+import { store } from './redux/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
-    <ChakraProvider theme={chakraTheme}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </ChakraProvider>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <ChakraProvider theme={chakraTheme}>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </ChakraProvider>
+    </BrowserRouter>
+  </Provider>,
 );
